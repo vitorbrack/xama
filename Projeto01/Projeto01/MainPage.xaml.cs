2ca728244c1cecc4ac822f06694a30fca1041763
vitorbrack <vitorodmiranda@gmail.com>
2021-09-22 08:44:38 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto01/Projeto01/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Projeto01
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btEnviar(object sender, EventArgs e)
+        {
+            DisplayAlert("Cuidado!", "Você está sendo hackeado", "OK");
+        }
+    }
+}
---
+++ Projeto01/Projeto01/Pagina02.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projeto01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina02 : ContentPage
+    {
+        public Pagina02()
+        {
+            InitializeComponent();
+
+            StackLayout stk = new StackLayout()
+            {
+                Orientation = StackOrientation.Vertical
+            };
+
+            Label lb1 = new Label()
+            {
+                Text = "Curso Xamarin",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 24,
+                BackgroundColor = Color.Black,
+                Padding = new Thickness(10),
+                TextColor = Color.White,
+                HorizontalTextAlignment = TextAlignment.Center,
+            };
+            stk.Children.Add(lb1);
+
+            Label lb2 = new Label()
+            {
+                Text = "Banco de dados",
+                FontSize = 20,
+                TextColor = Color.DarkBlue,
+            };
+
+            Label lb3 = new Label()
+            {
+
+                Text = "Java Web(EE)",
+                FontSize = 20,
+                TextColor = Color.DarkGreen,
+            };
+
+            StackLayout stk2 = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.Center,
+                Padding = new Thickness(20),
+                VerticalOptions = LayoutOptions.Center
+               
+            };
+
+            stk2.Children.Add(lb2);
+            stk2.Children.Add(lb3);
+
+            stk.Children.Add(stk2);
+
+            Image img = new Image()
+            { 
+                Source = "images.jpg",
+                HeightRequest = 150,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            stk.Children.Add(img);
+
+            Button bt1 = new Button()
+            {
+                Text = "Enviar",
+                Padding = new Thickness(15),
+                HeightRequest = 45,
+                WidthRequest = 200,
+                BackgroundColor = Color.Aqua,
+                TextColor = Color.Purple,
+                FontSize = 15,
+                Margin = new Thickness (20)
+            };
+            bt1.Clicked += Bt1_Clicked;
+            stk.Children.Add(bt1);
+
+            Content = stk;
+        }
+      
+    }
+}
